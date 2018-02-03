2e35baa962df80acd65ba16663c0a30ece5d966f
Zachary Cody <zacharytcody47@gmail.com>
2018-02-02 22:16:48 -0600

    changed robotmap for drivetrain

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -3,12 +3,12 @@
 
 //CANBus PIDs	NAME				PID
 constexpr int DRIVE_LEFTMOTOR_1 	= 1;
-constexpr int DRIVE_LEFTMOTOR_2 	= 2;
-constexpr int DRIVE_LEFTMOTOR_3 	= 3;
+constexpr int DRIVE_LEFTMOTOR_2 	= 3;
+constexpr int DRIVE_LEFTMOTOR_3 	= 5;
 
 
-constexpr int DRIVE_RIGHTMOTOR_1 	= 4;
-constexpr int DRIVE_RIGHTMOTOR_2 	= 5;
+constexpr int DRIVE_RIGHTMOTOR_1 	= 2;
+constexpr int DRIVE_RIGHTMOTOR_2 	= 4;
 constexpr int DRIVE_RIGHTMOTOR_3 	= 6;
 
 constexpr int PIGEON_GYRO 			= 7;
