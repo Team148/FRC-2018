d98780f4cc7ed3ebf20eaaef3773d7b39218c07e
Zachary Cody <zacharytcody47@gmail.com>
2018-02-03 15:46:05 -0600

    climber constant

--- src/Constants.h
+++ src/Constants.h
@@ -6,6 +6,6 @@
 #define CONSTANTS_H
 
 constexpr float CLIMBER_OUTPUT_PERCENT = 1.0;
-constexpr float ELEVATOR_OUTPUT_PERCENT = 1.0;
+constexpr float ELEVATOR_OUTPUT_PERCENT = 0.30;
 
 #endif //CONSTANTS_H
