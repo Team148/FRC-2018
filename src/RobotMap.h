1009038e14faaca4ab2b60ff414238992ee1c072
Caleb <35179811+CBry148@users.noreply.github.com>
2018-02-03 19:12:10 -0600

    Corrected RobotMap

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -2,26 +2,26 @@
 #define ROBOTMAP_H
 
 //CANBus PIDs	NAME				PID
-constexpr int DRIVE_LEFTMOTOR_1 	= 1;
-constexpr int DRIVE_LEFTMOTOR_2 	= 3;
-constexpr int DRIVE_LEFTMOTOR_3 	= 5;
+constexpr int DRIVE_LEFTMOTOR_1 	= 28;
+constexpr int DRIVE_LEFTMOTOR_2 	= 29;
+constexpr int DRIVE_LEFTMOTOR_3 	= 30;
 
-constexpr int DRIVE_RIGHTMOTOR_1 	= 2;
-constexpr int DRIVE_RIGHTMOTOR_2 	= 4;
-constexpr int DRIVE_RIGHTMOTOR_3 	= 6;
+constexpr int DRIVE_RIGHTMOTOR_1 	= 31;
+constexpr int DRIVE_RIGHTMOTOR_2 	= 32;
+constexpr int DRIVE_RIGHTMOTOR_3 	= 33;
 
-constexpr int INTAKE_MOTOR_1		= 8;
-constexpr int INTAKE_MOTOR_2		= 9;
+constexpr int INTAKE_MOTOR_1		= 1;
+constexpr int INTAKE_MOTOR_2		= 2;
 
-constexpr int ELEVATOR_MOTOR_1		= 10;
-constexpr int ELEVATOR_MOTOR_2		= 11;
+constexpr int ELEVATOR_MOTOR_1		= 9;
+constexpr int ELEVATOR_MOTOR_2		= 10;
 //constexpr int ELEVATOR_MOTOR_3	  = 11;
 
-constexpr int CLIMBER_MOTOR_1		= 12;
-constexpr int CLIMBER_MOTOR_2		= 13;
-constexpr int CLIMBER_MOTOR_3		= 14;
+constexpr int CLIMBER_MOTOR_1		= 11;
+constexpr int CLIMBER_MOTOR_2		= 12;
+constexpr int CLIMBER_MOTOR_3		= 13;
 
-constexpr int FORKLIFT_MOTOR_1		= 15;
+constexpr int FORKLIFT_MOTOR_1		= 16;
 
 constexpr int PIGEON_GYRO 			= 48;
 
