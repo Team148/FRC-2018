68e6f5d1b6cb852eaaccda8b9a397c30cf06ffa0
Caleb <35179811+CBry148@users.noreply.github.com>
2018-02-12 19:56:16 -0600

    Added Full System OI

--- src/Constants.h
+++ src/Constants.h
@@ -36,8 +36,9 @@ constexpr float MOTOR_REVERSE_FULL_OUTPUT = 1.0;
 
 constexpr float INTAKE_PERCENT = 0.8;
 constexpr float OUTTAKE_PERCENT = -0.30;
+constexpr float OUTTAKE_FULL_PERCENT = -1.0;
 constexpr float CLIMBER_OUTPUT_PERCENT = 1.0;
-constexpr float WRANGLER_OUTPUT_PERCENT = 1.0;
+constexpr float WRANGLER_OUTPUT_PERCENT = -1.0;
 constexpr float ELEVATOR_OUTPUT_PERCENT = 1.0;
 constexpr int LEFT_VALUE = -1;
 constexpr int RIGHT_VALUE = 1;
--- src/OI.h
+++ src/OI.h
@@ -18,6 +18,8 @@ private:
 	JoystickButton* m_drvButton6;
 	JoystickButton* m_drvButton7;
 	JoystickButton* m_drvButton8;
+	JoystickButton* m_drvButton9;
+	JoystickButton* m_drvButton10;
 
 	JoystickButton* m_opButton1;
 	JoystickButton* m_opButton2;
@@ -26,5 +28,7 @@ private:
 	JoystickButton* m_opButton5;
 	JoystickButton* m_opButton6;
 	JoystickButton* m_opButton7;
-
+	JoystickButton* m_opButton8;
+	JoystickButton* m_opButton9;
+	JoystickButton* m_opButton10;
 };
