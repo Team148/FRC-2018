0a3d3560551e09dd0416e94aa5af63d780fd9946
Zachary Cody <zacharytcody47@gmail.com>
2018-03-16 12:39:14 -0500

    changed parallel call for elevator to sequential after path(scale only auto)

--- src/Commands/Auto/AutoCommandGroups/AutonSelector_ScalePriority.cpp
+++ src/Commands/Auto/AutoCommandGroups/AutonSelector_ScalePriority.cpp
@@ -23,31 +23,32 @@ AutonSelector_ScalePriority::AutonSelector_ScalePriority(int start_pos, std::str
 {
 
 	std::string fms_data_truc = FMS_Data.substr (0,2);
+    std::cout << "What I See: " << fms_data_truc << std::endl;
+
 
 
 if(start_pos == tStartingPosition::RIGHT_POS)
 {
 	if(fms_data_truc.compare(autoConstData.R_R) == 0) // R POS RR
 	{
-	    std::cout << "What I See: " << fms_data_truc << std::endl;
 	   // AddSequential(new FromMiddlePos_ToRightScale());
 	    std::cout << FromRightPos_ToRightScalePath::GetInstance()->GetTimeLength() << std::endl;
 
-
-	    AddParallel(new AutoSetElevator(ELEVATOR_SCALE_HIGH, FromRightPos_ToRightScalePath::GetInstance()->GetTimeLength()-1.1));
+//	    AddParallel(new AutoSetElevator(ELEVATOR_SCALE_HIGH, FromRightPos_ToRightScalePath::GetInstance()->GetTimeLength()-1.1));
 	    AddSequential(new PathExecuter(FromRightPos_ToRightScalePath::GetInstance(), false));
+	    AddSequential(new AutoSetElevator(ELEVATOR_SCALE_HIGH, 0.0)); // ADDED, NOT NORMAL
 	    AddSequential(new WaitCommand(0.5));
 	    AddSequential(new AutoIntake(OUTTAKE_FULL_PERCENT, 0.3));
 	    AddSequential(new AutoSetElevator(ELEVATOR_ZERO, 0.0));
 	    AddSequential(new TurnPosition(170, 0.5));
 	    AddParallel(new AutoIntake(INTAKE_FAST_PERCENT, FromRightScale_ToRightFirstCubePath::GetInstance()->GetTimeLength()+3.0));
 	    AddSequential(new PathExecuter(FromRightScale_ToRightFirstCubePath::GetInstance(), false));
-	//    AddParallel(new AutoIntake(INTAKE_FAST_PERCENT, 3.5));
-	    AddParallel(new AutoSetElevator(ELEVATOR_SCALE_HIGH, FromRightFirstCube_ToRightScalePath::GetInstance()->GetTimeLength()-1.0));
-	//    AddParallel(new AutoIntake(INTAKE_SLOW_PERCENT, 5.0));
+	    								//    AddParallel(new AutoIntake(INTAKE_FAST_PERCENT, 3.5));
+//	    AddParallel(new AutoSetElevator(ELEVATOR_SCALE_HIGH, FromRightFirstCube_ToRightScalePath::GetInstance()->GetTimeLength()-1.0));
+	    								//    AddParallel(new AutoIntake(INTAKE_SLOW_PERCENT, 5.0));
 	    AddSequential(new PathExecuter(FromRightFirstCube_ToRightScalePath::GetInstance(), true));
 	    AddSequential(new TurnPosition(70, 0.5));
-//	    AddSequential(new AutoSetElevator(ELEVATOR_SCALE_HIGH, 0.0));
+	    AddSequential(new AutoSetElevator(ELEVATOR_SCALE_HIGH, 0.0)); // ADDED, NOT NORMAL
 	    AddSequential(new WaitCommand(0.2));
 	    AddSequential(new AutoIntake(OUTTAKE_FULL_PERCENT, 0.5));
 	    AddSequential(new AutoDrive(-15, 150, 0, 70));
