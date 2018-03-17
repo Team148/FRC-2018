0712c0224219afbf94fac6bf0cd4d773c30a0c50
James Tonthat <jamestonthat@gmail.com>
2018-03-16 23:33:51 -0500

    Fixed Degrees to Rads on AutoDrive on 118 Auto

--- src/Commands/Auto/AutoCommandGroups/AutonSelector_ScalePriority.cpp
+++ src/Commands/Auto/AutoCommandGroups/AutonSelector_ScalePriority.cpp
@@ -18,7 +18,7 @@
 #include "../AutoDrive.h"
 #include <iostream>
 
-
+#define RadianToDegrees(angleRadians) ((angleRadians) * 180 / M_PI)
 
 AutonSelector_ScalePriority::AutonSelector_ScalePriority(int start_pos, std::string FMS_Data, int cube_amount)
 {
@@ -62,33 +62,33 @@ if(start_pos == tStartingPosition::RIGHT_POS)
 	if(fms_data_truc.compare(autoConstData.R_L) == 0 || fms_data_truc.compare(autoConstData.L_L) == 0) // R POS RL
 	{
 
-	    if(OI::GetInstance()->GetSw4()) // 118 special mode activated
-	    {
-			AddParallel(new AutoIntake(OUTTAKE_MAX_PERCENT, 0.7));
-			AddSequential(new PathExecuter(FromRightPos_ToLeftScale118Path::GetInstance(), false)); //add me pls
-			AddSequential(new AutoSetElevator(ELEVATOR_SCALE_HIGH, 0.0));
-			AddSequential(new AutoIntake(OUTTAKE_FULL_PERCENT, 0.5));
-			AddSequential(new AutoSetElevator(ELEVATOR_ZERO, 0.0));
-			AddSequential(new AutoDrive(-25, 150, 0, FromRightPos_ToLeftScale118Path::GetInstance()->GetEndHeading()));
-
-	    }
-	    else //normal left scale case
-	    {
-			//AddSequential(new PathExecuter(FromRightPos_ToLeftScalePath::GetInstance(), false)); // add pls
-				AddSequential(new AutoSetElevator(ELEVATOR_SCALE_HIGH, 0.0)); // ADDED, NOT NORMAL
-			AddSequential(new WaitCommand(0.5));
-			AddSequential(new AutoIntake(OUTTAKE_FULL_PERCENT, 0.3));
-			AddSequential(new AutoSetElevator(ELEVATOR_ZERO, 0.0));
-			AddSequential(new TurnPosition(180, 1.2));
-			AddParallel(new AutoIntake(INTAKE_FAST_PERCENT, 5.0)); // ADD THE PATH TIME IN
-			//AddSequential(new PathExecuter(FromLeftScale_ToLeftFirstCubePath::GetInstance(), false)); // add pls
-			AddSequential(new TurnPosition(0, 1.2));
-			//AddSequential(new PathExecuter(FromLeftFirstCube_ToLeftScalePath::GetInstance(), false)); // add pls
-			AddSequential(new AutoSetElevator(ELEVATOR_SCALE_HIGH, 0.0)); // ADDED, NOT NORMAL
-			AddSequential(new WaitCommand(0.5));
-			AddSequential(new AutoIntake(OUTTAKE_FULL_PERCENT, 0.3));
-			AddSequential(new AutoSetElevator(ELEVATOR_ZERO, 0.0)); // ADDED, NOT NORMAL
-	    }
+//	    if(OI::GetInstance()->GetSw4()) // 118 special mode activated
+//	    {
+		//	AddParallel(new AutoIntake(OUTTAKE_MAX_PERCENT, 0.7));
+				AddSequential(new PathExecuter(FromRightPos_ToLeftScale118Path::GetInstance(), false)); //add me pls
+				AddSequential(new AutoSetElevator(ELEVATOR_SCALE_HIGH, 0.0));
+				AddSequential(new AutoIntake(OUTTAKE_FULL_PERCENT, 0.5));
+				AddSequential(new AutoSetElevator(ELEVATOR_ZERO, 0.0));
+				AddSequential(new AutoDrive(-15, 150, 0, RadianToDegrees(FromRightPos_ToLeftScale118Path::GetInstance()->GetEndHeading())));
+//
+//	    }
+//	    else //normal left scale case
+//	    {
+//			//AddSequential(new PathExecuter(FromRightPos_ToLeftScalePath::GetInstance(), false)); // add pls
+//				AddSequential(new AutoSetElevator(ELEVATOR_SCALE_HIGH, 0.0)); // ADDED, NOT NORMAL
+//			AddSequential(new WaitCommand(0.5));
+//			AddSequential(new AutoIntake(OUTTAKE_FULL_PERCENT, 0.3));
+//			AddSequential(new AutoSetElevator(ELEVATOR_ZERO, 0.0));
+//			AddSequential(new TurnPosition(180, 1.2));
+//			AddParallel(new AutoIntake(INTAKE_FAST_PERCENT, 5.0)); // ADD THE PATH TIME IN
+//			//AddSequential(new PathExecuter(FromLeftScale_ToLeftFirstCubePath::GetInstance(), false)); // add pls
+//			AddSequential(new TurnPosition(0, 1.2));
+//			//AddSequential(new PathExecuter(FromLeftFirstCube_ToLeftScalePath::GetInstance(), false)); // add pls
+//			AddSequential(new AutoSetElevator(ELEVATOR_SCALE_HIGH, 0.0)); // ADDED, NOT NORMAL
+//			AddSequential(new WaitCommand(0.5));
+//			AddSequential(new AutoIntake(OUTTAKE_FULL_PERCENT, 0.3));
+//			AddSequential(new AutoSetElevator(ELEVATOR_ZERO, 0.0)); // ADDED, NOT NORMAL
+//	    }
 	}
 }
 
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -149,8 +149,8 @@ public:
 				autoPosition = tStartingPosition::RIGHT_POS;
 			}
 
-			frc::Scheduler::GetInstance()->AddCommand(new AutonSelector_Hybrid(autoPosition, gameData, cubeAmount));
-	//        frc::Scheduler::GetInstance()->AddCommand(new AutonSelector_ScalePriority(autoPosition, gameData, cubeAmount));
+	//		frc::Scheduler::GetInstance()->AddCommand(new AutonSelector_Hybrid(autoPosition, gameData, cubeAmount));
+	        frc::Scheduler::GetInstance()->AddCommand(new AutonSelector_ScalePriority(autoPosition, gameData, cubeAmount));
 	//        frc::Scheduler::GetInstance()->AddCommand(new AutonSelector_SwitchOnly(tStartingPosition::MIDDLE_POS, gameData, cubeAmount));
 
 
